8c2a049d33770be2bed67da45317eb928b78cf96
johnhaas <haasj22@up.edu>
2022-11-05 12:45:23 -0700

    Room Chunk variables

--- GlobalStats.gd
+++ GlobalStats.gd
@@ -4,8 +4,14 @@ extends Node
 var dimension = 2
 var mainLocation = [0,0]
 
-var map = [["M", "H"],
-				["H", "V"]]
+"""
+M - Main 
+H - Horizontal
+V - Vertical
+X - XRoom
+"""
+var map = [[preload('res://RoomChunk.gd').new().set_Params(["M", false, false]), preload('res://RoomChunk.gd').new().set_Params(["H", false, false])],
+				[preload('res://RoomChunk.gd').new().set_Params(["H", false, false]), preload('res://RoomChunk.gd').new().set_Params(["V", false, true])]]
 		
 
 var curPos = [mainLocation[0], mainLocation[1]]
--- RoomChunk.gd
+++ RoomChunk.gd
@@ -2,5 +2,29 @@ extends Node2D
 
 class_name RoomChunk
 
+export(String) var type = "" setget set_Type, get_Type
+export(bool) var enemyAlive = false setget set_Enemy_Alive, get_Enemy_Alive
+export(bool) var objectiveInRoom = false setget set_Objective_In_Room, get_Objective_In_Room
+
+func set_Type(var Type):
+	type = Type
+func get_Type():
+	return type
+
+func set_Enemy_Alive(var EnemyAlive):
+	enemyAlive = EnemyAlive;
+func get_Enemy_Alive():
+	return enemyAlive
+
+func set_Objective_In_Room(var ObjectiveInRoom):
+	objectiveInRoom = ObjectiveInRoom
+func get_Objective_In_Room():
+	return objectiveInRoom
+
+func set_Params(var Params):
+	type = Params[0]
+	enemyAlive = Params[1]
+	objectiveInRoom = Params[2]
+
 func decideNextRoom(currPos):
 	pass
